3f1e6b1e77eb5bfc7ed5b19171fdaf33176557a4
Pysched <murphyjdarren@gmail.com>
2020-07-21 22:47:34 +0000

    updated products.html

--- products/views.py
+++ products/views.py
@@ -5,10 +5,12 @@ from .models import Product
 
 
 def all_products(request):
-    """A view to show all products, including sorting and search queries"""
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
 
     context = {
         'products': products,
     }
+
     return render(request, 'products/products.html', context)
