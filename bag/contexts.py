0bd0ca54aef1b1634bfb7baf26b4a77ec00bff36
Pysched <murphyjdarren@gmail.com>
2020-07-23 14:42:46 +0000

    updated settings.py for bag references

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,7 +9,7 @@ def bag_contents(request):
     product_count = 0
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PRECENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
@@ -23,7 +23,7 @@ def bag_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_deliverythreshold': settings.FREE_DELIVERY_THRESHOLD,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
