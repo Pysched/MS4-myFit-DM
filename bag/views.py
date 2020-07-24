b2eac02d7f7790e72d06348f6448a823e7561f27
Pysched <murphyjdarren@gmail.com>
2020-07-24 21:31:54 +0000

    adjusting bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 # Create your views here.
@@ -13,26 +13,22 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
     if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
         else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
+            del bag[item_id]['items_by_size'][size]
     else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-        else:
+        if quantity > 0:
             bag[item_id] = quantity
+        else:
+            bag.pop[item_id]
 
     request.session['bag'] = bag
-    return redirect(redirect_url)
+    return redirect(reverse('view_bag'))
