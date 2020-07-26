28ba72bf2c6cba564035b1b533708f8e3f37156c
Pysched <murphyjdarren@gmail.com>
2020-07-26 13:33:02 +0000

    updated user profiles

--- profiles/models.py
+++ profiles/models.py
@@ -23,4 +23,6 @@ class UserProfile(models.Model):
 def create_or_update_user_prodile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-        instance.userprofile.save()
+
+    instance.userprofile.save()
+       
